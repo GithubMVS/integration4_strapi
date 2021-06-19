dabcc275d0c2050e79dae16550c0b60be344b334
Matisse <matisse.van.steenbergen@student.howest.be>
2021-06-19 20:26:41 +0200

    small changes

--- config/database.js
+++ config/database.js
@@ -1,20 +1,18 @@
-const parse = require('pg-connection-string').parse;
-
-module.exports = ({ env }) => {
-
-  return {
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'sqlite',
-          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-        },
-        options: {
-          useNullAsDefault: true,
-        },
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "mysql",
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 8889),
+        database: ezv("DATABASE_NAME", "this-is-random"),
+        username: env("DATABASE_USERNAME", "root"),
+        password: env("DATABASE_PASSWORD", "root"),
+        ssl: env.bool("DATABASE_SSL", false),
       },
+      options: {},
     },
-  }
-};
+  },
+});
