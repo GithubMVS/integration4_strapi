1252231400aa77029de32e4a6209d7a18ebb8cd4
Matisse <matisse.van.steenbergen@student.howest.be>
2021-06-20 16:12:34 +0200

    small changes with config vars

--- config/database.js
+++ config/database.js
@@ -1,38 +1,3 @@
-/*
-const parse = require('pg-connection-string').parse;
-
-module.exports = ({ env }) => {
-
-  if (env('NODE_ENV') === 'production') {
-    const config = parse(process.env.DATABASE_URL)
-    return {
-      defaultConnection: 'default',
-      connections: {
-
-      }
-    }
-  }
-
-  return {
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'sqlite',
-          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-        },
-        options: {
-          useNullAsDefault: true,
-        },
-      },
-    },
-  }
-};
-*/
-
-
-
 module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
@@ -40,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: "bookshelf",
       settings: {
         client: "mysql",
-        host: env("PHP_DB_HOST", "127.0.0.1"),
+        host: env("DATABASE_HOST", "127.0.0.1"),
         port: env.int("DATABASE_PORT", 8889),
-        database: env("PHP_DB_DATABASE", "this-is-random"),
-        username: env("PHP_DB_PASSWORD", "root"),
-        password: env("PHP_DB_USERNAME", "root"),
+        database: ezv("DATABASE_NAME", "this-is-random"),
+        username: env("DATABASE_USERNAME", "root"),
+        password: env("DATABASE_PASSWORD", "root"),
         ssl: env.bool("DATABASE_SSL", false),
       },
       options: {},
